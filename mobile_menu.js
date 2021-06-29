ea7964afce9ec0312ea0b4a392fc56f0796f95e1
Marc Raphael <marcraphael979@gmail.com>
2021-06-29 10:13:37 +0100

    add nav for mobile menu

--- mobile_menu.js
+++ mobile_menu.js
@@ -8,27 +8,26 @@ function mobileMenu() {
   menuContainer.style.width = '100%';
   menuContainer.style.margin = '0 0 50px 0'
   menuContainer.style.listStyleType = 'unset'
-  menuContainer.style.backgroundColor = 'white';
+  menuContainer.style.backgroundColor = 'rgba(255, 255, 255, 1)';
   menuContainer.style.backgroundImage = 'url(./mobile-menu-image/image_geometry_menu_1.png), url(./mobile-menu-image/image_geometry_menu_2.png)';
   menuContainer.style.backgroundPosition = 'top left, bottom';
   menuContainer.style.backgroundRepeat = 'no-repeat, no-repeat';
   menuContainer.style.backgroundSize = 'auto auto, 100% auto';
   menuContainer.style.fontFamily = '"Poppins", sans-serif';
   menuContainer.style.color = 'rgba(26, 34, 54, 1)';
+  //menucontainer.style.display = 'grid';
+  menuContainer.style.position = 'fixed';
+  menuContainer.style.zIndex = '2';
 
   const logo = document.querySelector('.class-my-logo');
   logo.style.display = 'none';
 
   hamburger.src = './mobile-menu-image/Union.png';
-  
+  hamburger.style.gridColumn = '1 / 2';
+  hamburger.style.alignSelf = 'initial';
 
-  //document.body.appendChild(menuContainer);
-
-  // create menu links
-  // let link1 = document.createElement('a').innerHTML = 'Hello';
-  // let link2 = document.createElement('a').innerHTML = 'Portofolio';
-  ///let link3 = document.createElement('a').innerHTML = 'About';
-  // let link4 = document.createElement('a').innerHTML = 'Contact';
+  const links = document.querySelectorAll('header.class-header-link');
+  links.style.display = 'initial';
 }
 
 hamburger.addEventListener('click', mobileMenu);
