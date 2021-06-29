96ebf78a72f7babef8648a4b413e87f18be1475f
Marc Raphael <marcraphael979@gmail.com>
2021-06-29 01:19:03 +0100

    update menu container

--- mobile_menu.js
+++ mobile_menu.js
@@ -1,21 +1,28 @@
 const hamburger = document.querySelector('.class-side-menu');
-hamburger.addEventListener('click', mobileMenu);
+
 
 function mobileMenu() {
   // create menu element
-  let menuContainer = document.createElement('menu');
+  const menuContainer = document.getElementById('id-header-content');
   menuContainer.style.height = '568px';
-  menuContainer.style.width = '320';
+  menuContainer.style.width = '100%';
+  menuContainer.style.margin = '0 0 50px 0'
+  menuContainer.style.listStyleType = 'unset'
   menuContainer.style.backgroundColor = 'white';
-  menuContainer.style.backgroundImage = 'url(./mobile-menu-image/image_geometry_menu_1.png), url(./mobile-menu-image/image_geometry_menu_1.png)';
-  menuContainer.style.backgroundPosition = 'top left, bottom center';
-  menuContainer.style.backgroundRepeat ='no-repeat, no-repeat';
+  menuContainer.style.backgroundImage = 'url(./mobile-menu-image/image_geometry_menu_1.png), url(./mobile-menu-image/image_geometry_menu_2.png)';
+  menuContainer.style.backgroundPosition = 'top left, bottom';
+  menuContainer.style.backgroundRepeat = 'no-repeat, no-repeat';
+  menuContainer.style.backgroundSize = 'auto auto, 100% auto';
   menuContainer.style.fontFamily = '"Poppins", sans-serif';
   menuContainer.style.color = 'rgba(26, 34, 54, 1)';
 
+  //document.body.appendChild(menuContainer);
+
   // create menu links
-  let link1 = document.createElement('a').createTextNode('Hello');
-  let link2 = document.createElement('a').createTextNode('Portofolio');
-  let link3 = document.createElement('a').createTextNode('About');
-  let link4 = document.createElement('a').createTextNode('Contact');
+  // let link1 = document.createElement('a').innerHTML = 'Hello';
+  // let link2 = document.createElement('a').innerHTML = 'Portofolio';
+  ///let link3 = document.createElement('a').innerHTML = 'About';
+  // let link4 = document.createElement('a').innerHTML = 'Contact';
 }
+
+hamburger.addEventListener('click', mobileMenu);
