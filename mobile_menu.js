43289f880e99b585a33d0a688b4c95d16dee6184
Marc Raphael <marcraphael979@gmail.com>
2021-07-19 19:07:24 +0100

    add update

--- mobile_menu.js
+++ mobile_menu.js
@@ -1,37 +1,73 @@
-const hdr = document.querySelector('#header')
-const btn = document.querySelector('.menu-btn')
+const hdr = document.querySelector('#header') // the header element
+const btn = document.querySelector('.menu-btn') // the opening button
 const menu = document.querySelector('.menu')
 const sect1 = document.querySelector('#section1')
-const link = document.querySelectorAll('.link')
+const link = document.querySelectorAll('.link') // Each link element class
 
+/********* The closig button ************/
 const cross = document.createElement('button')
-cross.setAttribute('style', 'margin: 50px 50px 50px auto; width: max-content; background: none; font-weigth: 500; font-size: 20px; border: none; cursor: pointer;');
+cross.setAttribute('style', '\
+margin: 30px 30px 50px auto;\
+width: max-content;\
+background: none;\
+font-weight: 500;\
+font-size: 20px;\
+border: none;\
+cursor: pointer;');
 cross.innerHTML = '&times;';
 
+/*********** The opening function ***************/
 function open() {
-  hdr.setAttribute('style', 'height: 100vh; width: 100%; background-image: url(./mobile-menu-image/image_geometry_menu_1.png), url(./mobile-menu-image/image_geometry_menu_2.png); background-repeat: no-repeat; background-position: left top, center bottom; margin-top: 0; position: fixed; background-color: white; flex-direction: column-reverse; justify-content: start;');
+
+  hdr.setAttribute('style', '\
+  height: 100vh;\
+  width: 100%;\
+  background-image: url(./mobile-menu-image/image_geometry_menu_1.png),\
+  url(./mobile-menu-image/image_geometry_menu_2.png);\
+  background-repeat: no-repeat;\
+  background-position: left top, center bottom;\
+  margin-top: 0;\
+  position: fixed;\
+  background-color: white;\
+  flex-direction: column-reverse;\
+  justify-content: flex-end;');
+
   btn.setAttribute('style', 'display: none');
   sect1.style.marginTop = '0';
-  menu.setAttribute('style', 'display: flex; flex-direction: column; align-items: center; align-self: center; margin-top: 20px;');
-  for (i = 0; i < link.length; ++i)
-    { link[i].setAttribute('style', 'margin-left: 0; margin-top: 20px; color: #35418c;'); }
+  menu.setAttribute('style', '\
+  display: flex;\
+  flex-direction: column;\
+  align-items: center;\
+  align-self: center; margin-top: 20px;');
 
-  hdr.appendChild(cross)
+  // Apply style on every link element
+  for (i = 0; i < link.length; ++i)
+    { link[i].setAttribute('style', '\
+    margin-left: 0;\
+    margin-top: 20px;\
+    color: #35418c;'); }
+
+  hdr.appendChild(cross) // add cross to the header
 }
 
-btn.addEventListener('click', open);
+btn.addEventListener('click', open); // calling the opening function
 
+/************* The closing function ****************/
 function close() {
-  hdr.removeAttribute('style');
-  menu.removeAttribute('style');
+  hdr.removeAttribute('style'); //remove the style attribute
+  menu.removeAttribute('style');//remove the style attribute
   sect1.style.marginTop = '70px';
-  btn.removeAttribute('style');
-  hdr.removeChild(cross);
-  for (i = 0; i < link.length; ++i)
-    { link[i].setAttribute('style', 'margin-left: 20px; margin-top: 0;') }
+  btn.removeAttribute('style'); //remove the style attribute
+  hdr.removeChild(cross); // remove cross from header
+
+  // reset the link element style
+  for (i = 0; i < link.length; ++i) 
+    { link[i].setAttribute('style', '\
+    margin-left: 20px;\
+    margin-top: 0;') }
 }
 
-cross.addEventListener('click', close)
+cross.addEventListener('click', close) // calling the closing function and reset the header
 
 for (i = 0; i < link.length; ++i)
   { link[i].addEventListener('click', close) } // close menu when clicking on link
--- projects_mobile_desktop.js
+++ projects_mobile_desktop.js
@@ -3,33 +3,58 @@ const technologiesUsed = {
   htmlCss : 'HTML/CSS',
   JavaScript : 'JavaScript',
   ruby : 'Ruby',
-  technologies : []
 }
 
-
-
-const techs = technologiesUsed.technologies
-
-for (i = 0; i < 3; ++i) {
-  const tech = document.createElement('li')
+function techStyle(tech) {
   tech.setAttribute('style', '\
-  list-style-type: none;\
-  width: max-content; height: auto;\
-  padding: 10px;\
-  border 1px solid #1A2236;')
-
-  techs[i] = tech
+  display: flex;\
+  align-items: center;\
+  justify-content: center;\
+  padding: 6px 10px;\
+  font-size: 11px;\
+  font-weight: 300;\
+  margin-right: 10px;\
+  border: solid 0.5px #1A2236;\
+  color: #1A2236;')
+  return tech
 }
+
+/***** Technologies list ****/
+const tech1 = document.createElement('li')
+tech1.innerHTML = technologiesUsed.htmlCss
+techStyle(tech1)
+
+const tech2 = document.createElement('li')
+tech2.innerHTML = technologiesUsed.JavaScript
+techStyle(tech2)
+
+const tech3 = document.createElement('li')
+tech3.innerHTML = technologiesUsed.ruby
+techStyle(tech3)
+
+const techs = document.createElement('ul')
+techs.setAttribute('style', '\
+list-style-type: none;\
+display: flex;\
+width: 100%;\
+margin-bottom: 18px;\
+height: auto;')
+
+techs.appendChild(tech1)
+techs.appendChild(tech2)
+techs.appendChild(tech3)
 
 /******** Images library **********/
 const images = {
   image1 : document.createElement('img')
+  // can add more imeges later
 }
 
 function imagesStyle(img) {
   img.setAttribute('style','\
-  width: 100%;\
-  height: auto;')
+  width: 93%;\
+  height: auto;\
+  margin: auto;')
   return img
 }
 
@@ -39,6 +64,8 @@ imagesStyle(img1)
 
 /******** Texts library **********/
 const texts = {
+  title: 'Project name goes here',
+
   text1 :'	Lorem ipsum dolor sit amet consectetur,\
 	adipisicing elit. Illum, sed doloribus praesentium\
 	aperiam voluptate facilis maxime quos similique nihil saepe,\
@@ -50,6 +77,15 @@ const texts = {
 	qui soluta! Ad minima, architecto nostrum earum nobis id provident?',
 }
 
+const projectName = document.createElement('h2')
+projectName.innerHTML = texts.title
+projectName.setAttribute('style', '\
+width: 100%;\
+height: auto;\
+font-weight: 700;\
+color: #1A2236;\
+margin-bottom: 23px;')
+
 function textsStyle(txt) {
   txt.setAttribute('style', '\
   font-family: "Poppins", sans-serif;\
@@ -57,7 +93,7 @@ function textsStyle(txt) {
   font-size: 12px;\
   line-height: 21px;\
   font-weight: 400;\
-  width: 97%;\
+  max-width: 700px;\
   height: auto;\
   margin: auto;\
   margin-block-start: 10px;\
@@ -73,6 +109,74 @@ const txt2 = document.createElement('p')
 txt2.innerHTML = texts.text2
 textsStyle(txt2)
 
+/******************** Project buttons **********************/
+const projectButtons = {
+  button1 : 'See live',
+  button2 : 'See source',
+  icon1 : document.createElement('img'),
+  icon2 : document.createElement('img')
+}
+
+/////// Only for icons ///////
+function iconsStyle(icn) {
+  icn.setAttribute('style', '\
+  margin-left: 36px;\
+  margin-right: 15.5px')
+
+  return icn
+}
+
+const live = projectButtons.icon1
+live.setAttribute('src', './linkpics/ic_union.svg')
+iconsStyle(live)
+
+const gitHub = projectButtons.icon2
+gitHub.setAttribute('src', './linkpics/ic_github.svg')
+iconsStyle(gitHub)
+
+///////// The links style //////////
+function buttonLinksStyle(txt) {
+  txt.setAttribute('style', '\
+  width: 246px;\
+  height: 51px;\
+  color: white;\
+  font-size: 18px;\
+  background: #1A2236;\
+  font-weight: 300;\
+  border: 1px solid #1A2236;\
+  display: flex;\
+  align-items: center;\
+  padding: 12px 15px;\
+  justify-content: flex-end;\
+  cursor: pointer;\
+  margin: 15px;')
+
+  return txt
+}
+
+const seeLive = document.createElement('a')
+seeLive.innerHTML = projectButtons.button1
+buttonLinksStyle(seeLive)
+seeLive.appendChild(live)
+
+const seeSource = document.createElement('a')
+seeSource.innerHTML = projectButtons.button2
+buttonLinksStyle(seeSource)
+seeSource.appendChild(gitHub)
+
+const Buttons = document.createElement('div')
+Buttons.setAttribute('style', '\
+max-width: 555px;\
+height: auto;\
+display: flex;\
+flex-direction: row;\
+flex-wrap: wrap;\
+justify-content: center;\
+margin: 29px auto 0 auto;')
+
+Buttons.appendChild(seeLive)
+Buttons.appendChild(seeSource)
+
 /******************* Open modal button: see this project *****************/
 const openModal = document.querySelector('.see-btn')
 
@@ -86,44 +190,51 @@ closeModal.setAttribute('style', '\
   border: none;\
   cursor: pointer;\
   align-self: flex-end;\
-  margin: 0 5px;\
+  margin: 19px 0;\
   color: rgba(226, 80, 92, 1);');
 closeModal.innerHTML = '&times;';
 
 /*********** The card element *********/
 const card = document.createElement('div')
 card.setAttribute('style','\
-height: calc(100% - 50px);\
-width: calc(100% - 50px);\
-max-width: 600px;\
+height: auto;\
+width: 100%;\
 background: white;\
 display: flex;\
 margin: 0 auto;\
+padding: 0 10px;\
+align-items: center;\
 flex-direction: column;')
+
+card.appendChild(closeModal)
+card.appendChild(projectName)
 card.appendChild(techs)
 card.appendChild(img1)
 card.appendChild(txt1)
 card.appendChild(txt2)
+card.appendChild(Buttons)
 
 /*********** The modal element *********/
-const modal = document.createElement('aside')
+const modal = document.createElement('aside') // Consider modal as an aside element
 modal.setAttribute('style','\
 height: 100%;\
 width: 100%;\
-background: rgba(26, 34, 54, 0.8);\
+background: rgba(26, 34, 54, 1);\
 position: fixed;\
 top: 0;\
 left: 0;\
 display: flex;\
-flex-direction: column;')
-modal.appendChild(closeModal)
+flex-direction: column;\
+overflow: scroll;')
 modal.appendChild(card)
 
+/************ Window object: screen size by using JavaScript Media method ************/
+
+/******* Opening and closing functions part *********/
 function openProject() { // open the modal
   document.body.appendChild(modal)
 }
 openModal.addEventListener('click', openProject)
-
 
 function closeProject() { //close the modal
   document.body.removeChild(modal)
