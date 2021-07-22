59a6503cf10efa15fdd2c295bcb927a535272304
Marc Raphael <marcraphael979@gmail.com>
2021-07-22 19:13:41 +0100

    fix linters errors

--- mobile_menu.js
+++ mobile_menu.js
@@ -1,11 +1,13 @@
-const hdr = document.querySelector('#header') // the header element
-const btn = document.querySelector('.menu-btn') // the opening button
-const menu = document.querySelector('.menu')
-const sect1 = document.querySelector('#section1')
-const link = document.querySelectorAll('.link') // Each link element class
+/* eslint-disable no-plusplus */
+/* eslint-disable no-multi-str */
+const hdr = document.querySelector('#header'); // the header element
+const btn = document.querySelector('.menu-btn'); // the opening button
+const menu = document.querySelector('.menu');
+const sect1 = document.querySelector('#section1');
+const link = document.querySelectorAll('.link'); // Each link element class
 
-/********* The closig button ************/
-const cross = document.createElement('button')
+/** ******* The closig button *********** */
+const cross = document.createElement('button');
 cross.setAttribute('style', '\
 margin: 30px 30px 50px auto;\
 width: max-content;\
@@ -16,9 +18,8 @@ border: none;\
 cursor: pointer;');
 cross.innerHTML = '&times;';
 
-/*********** The opening function ***************/
+/** ********* The opening function ************** */
 function open() {
-
   hdr.setAttribute('style', '\
   height: 100vh;\
   width: 100%;\
@@ -41,33 +42,34 @@ function open() {
   align-self: center; margin-top: 20px;');
 
   // Apply style on every link element
-  for (i = 0; i < link.length; ++i)
-    { link[i].setAttribute('style', '\
+  for (let i = 0; i < link.length; ++i) {
+    link[i].setAttribute('style', '\
     margin-left: 0;\
     margin-top: 20px;\
-    color: #35418c;'); }
+    color: #35418c;');
+  }
 
-  hdr.appendChild(cross) // add cross to the header
+  hdr.appendChild(cross); // add cross to the header
 }
 
 btn.addEventListener('click', open); // calling the opening function
 
-/************* The closing function ****************/
+/** *********** The closing function *************** */
 function close() {
-  hdr.removeAttribute('style'); //remove the style attribute
-  menu.removeAttribute('style');//remove the style attribute
+  hdr.removeAttribute('style'); // remove the style attribute
+  menu.removeAttribute('style');// remove the style attribute
   sect1.style.marginTop = '70px';
-  btn.removeAttribute('style'); //remove the style attribute
+  btn.removeAttribute('style'); // remove the style attribute
   hdr.removeChild(cross); // remove cross from header
 
   // reset the link element style
-  for (i = 0; i < link.length; ++i) 
-    { link[i].setAttribute('style', '\
+  for (let i = 0; i < link.length; ++i) {
+    link[i].setAttribute('style', '\
     margin-left: 20px;\
-    margin-top: 0;') }
+    margin-top: 0;');
+  }
 }
 
-cross.addEventListener('click', close) // calling the closing function and reset the header
+cross.addEventListener('click', close); // calling the closing function and reset the header
 
-for (i = 0; i < link.length; ++i)
-  { link[i].addEventListener('click', close) } // close menu when clicking on link
+for (let i = 0; i < link.length; ++i) { link[i].addEventListener('click', close); } // close menu when clicking on link
